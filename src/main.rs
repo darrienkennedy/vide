bb19793741a0d96f8f1a65ea33a66e1d078bb24a
Darrien Kennedy <darrienkennedydev@gmail.com>
2022-06-10 20:54:56 -0400

    added wrapper for vim

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::env;
+use std::process;
+
+const USAGE: &str = "vide [filepath]";
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() == 1 {
+        print_usage();
+        process::exit(1);
+    }
+
+    let mut editor = process::Command::new("vim").arg(&args[1]).spawn().unwrap();
+    editor.wait().unwrap();
+}
+
+fn print_usage() {
+    println!("Usage: {}", USAGE);
 }
