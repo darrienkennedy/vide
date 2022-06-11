93bc73d0e04917455be57505b6b91d7ecf31219a
darrienkennedy <101836681+darrienkennedy@users.noreply.github.com>
2022-06-10 20:57:57 -0400

    basic wrapper for vim (#2)
    * added wrapper for vim
    
    * imported process::Command directly

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::env;
+use std::process;
+use std::process::Command;
+
+const USAGE: &str = "vide [filepath]";
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() == 1 {
+        print_usage();
+        process::exit(1);
+    }
+
+    let mut editor = Command::new("vim").arg(&args[1]).spawn().unwrap();
+    editor.wait().unwrap();
+}
+
+fn print_usage() {
+    println!("Usage: {}", USAGE);
 }
