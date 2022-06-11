b805aa75845cf1c85fb3c3341feac48f21bb6b72
Darrien Kennedy <darrienkennedydev@gmail.com>
2022-06-10 20:44:07 -0400

    Added ability to specify a single filepath argument

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,20 @@
-use std::process::Command;
+use std::env;
+use std::process;
 
-const TARGET_FILE: &str = "/tmp/test";
+const USAGE: &str = "vide [filepath]";
 
 fn main() {
-    let mut editor = Command::new("vim").arg(TARGET_FILE).spawn().unwrap();
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() == 1 {
+        print_usage();
+        process::exit(1);
+    }
+
+    let mut editor = process::Command::new("vim").arg(&args[1]).spawn().unwrap();
     editor.wait().unwrap();
+}
+
+fn print_usage() {
+    println!("Usage: {}", USAGE);
 }
